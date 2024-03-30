fad897fadf77f11a946690126af2f32e600755f9
sriharshabasiri <harshabasiri@gmail.com>
2024-03-30 15:23:19 -0700

    f1

--- src/main/java/com/coop/verticles/FlagTransactionVerticle.java
+++ src/main/java/com/coop/verticles/FlagTransactionVerticle.java
@@ -1,11 +1,23 @@
 package com.coop.verticles;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.eventbus.MessageConsumer;
+import io.vertx.core.json.JsonObject;
 
 public class FlagTransactionVerticle extends AbstractVerticle {
 
     @Override
     public void start() throws Exception {
-        System.out.println("Flag Transaction Verticle");
+      vertx.setTimer(2000, handler->{
+          System.out.println("waiting for incoming message");
+
+          System.out.println("getting the message");
+          vertx.eventBus().consumer("flagtransaction", fdMessage -> {
+              System.out.println("msg recieved"+fdMessage.body());
+              JsonObject reply = new JsonObject();
+              reply.put("message","recieved");
+              fdMessage.reply(reply);
+          });
+      })  ;
     }
 }
